015161640c6bff6aaa5daa3dd915a994b0a2a774
bgallois <benjamin@gallois.cc>
2021-06-03 12:23:51 +0200

    [add] add build commands

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
 wget https://github.com/FastTrackOrg/FastTrack/archive/refs/heads/master.zip
 unzip master.zip
 rm master.zip
+mkdir FastTrack/docs
 cp -r FastTrack-master/docs/user/* FastTrack/docs/
 rm -r FastTrack-master
+cd FastTrack
+mkdir static/assets
+cp docs/assets/*.webm static/assets
+npm run build
+
