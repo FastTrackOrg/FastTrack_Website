281e1418688c4c20f5e153aa86c78c7a43e2e93e
bgallois <benjamin@gallois.cc>
2021-11-10 11:14:47 +0100

    [add] add API in build process

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,16 @@
 wget https://github.com/FastTrackOrg/FastTrack/archive/refs/heads/master.zip
 unzip master.zip
 rm master.zip
-mkdir FastTrack/docs
+mkdir -p FastTrack/docs
 cp -r FastTrack-master/docs/user/* FastTrack/docs/
+mkdir -p FastTrack/static/assets
+cp docs/assets/*.webm static/assets
+mkdir -p FastTrack/static/API
+cd FastTrack-master
+./generateDocumentation.sh
+cd ..
+cp -r FastTrack-master/manual/dev/html/html/* FastTrack/static/API/
 rm -r FastTrack-master
 cd FastTrack
-mkdir static/assets
-cp docs/assets/*.webm static/assets
 npm run build
 
